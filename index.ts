e12b56c677c0de4dc285bc828a2bca2768367dfb
alexis <munozmiguelalexis@gmail.com>
2021-09-09 17:52:51 -0300

    env vars

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 import * as express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
+import "dotenv/config";
+
+console.log(process.env.NODE_ENV);
+console.log(process.env.DB_HOST);
+
+app.get("/env", (req, res) => {
+  res.json({
+    enviorment: process.env.NODE_ENV,
+  });
+});
 
 app.get("/hola", (req, res) => {
   res.json({
-    message: "Hola soy Alex",
+    message: "hola soy alex",
   });
 });
 
 app.listen(port, () => {
-  console.log("hola soy express y estoy corriendo en el puero" + port);
+  console.log("hola soy express y estoy corriendo en el puero " + port);
 });
